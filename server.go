5f5c768fe94366db9dfcc98500a73eb405c07431
charles <charles.d.burton@gmail.com>
2015-11-06 11:30:23 -0700

    Created new handlers.  Added the html directory to hold the website info and built all of the necessary routes to make it work

--- server.go
+++ server.go
@@ -5,18 +5,50 @@ import (
     "github.com/gorilla/mux"
     "automata/handlers"
     "io/ioutil"
+    "bytes"
     "log"
 )
 
 
 func main() {
     rtr := mux.NewRouter()
+    rtr.HandleFunc("/", handleIndex).Methods("GET")
+    rtr.HandleFunc("/css/{rest:.*}", handleCSS).Methods("GET")
+    rtr.HandleFunc("/font-awesome/{rest:.*}", handleFontAwesome).Methods("GET")
+    rtr.HandleFunc("/js/{rest:.*}", handleJS).Methods("GET")
+    rtr.HandleFunc("/fonts/{rest:.*}", handleFonts).Methods("GET")
     rtr.HandleFunc("/devices/smoker", handleSmoker).Methods("POST", "GET")
     rtr.HandleFunc("/devices/temp-sensor", handleTempSensor).Methods("POST", "GET")
+    http.Handle("/", rtr)
 
     // Bind to a port and pass our router in
     log.Println("Listening on port 8000")
     log.Fatal(http.ListenAndServe(":8000", rtr))
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "html/")
+}
+
+func handleCSS(w http.ResponseWriter, r *http.Request) {
+	path := mungeString("html/css/", mux.Vars(r)["rest"])
+	http.ServeFile(w, r, path)
+}
+
+func handleFontAwesome(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "html/font-awesome/")
+}
+
+func handleJS(w http.ResponseWriter, r *http.Request) {
+	lead := "html/js/"
+	tail := mux.Vars(r)["rest"]
+	path := mungeString(lead, tail)
+	http.ServeFile(w,r, path)
+
+}
+
+func handleFonts(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "html/fonts/")
 }
 
 func handleSmoker(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +67,12 @@ func handleTempSensor(w http.ResponseWriter, r *http.Request) {
     }
     go handlers.TempSensorHandler(body)
     w.Write([]byte("OK\n"))
+}
+
+func mungeString(lead string, tail string) string{
+	var buffer bytes.Buffer
+	buffer.WriteString(lead)
+	buffer.WriteString(tail)
+	log.Println(buffer.String())
+	return buffer.String()
 }
